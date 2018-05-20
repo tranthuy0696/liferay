53cde05e52c6cb75445c2a2cefd40b0027b90007
nguyenngocbao <ngocbaostar787@gmail.com>
2018-05-20 23:39:51 +0700

    refactor git

---
+++ docroot/WEB-INF/src/com/monitor/portlet/monitorAction.java
@@ -0,0 +1,48 @@
+package com.monitor.portlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.management.ManagementFactory;
+
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class monitorAction
+ */
+public class monitorAction extends MVCPortlet {
+	com.sun.management.OperatingSystemMXBean os = (com.sun.management.OperatingSystemMXBean) ManagementFactory
+			.getOperatingSystemMXBean();
+
+	@Override
+	public void serveResource(ResourceRequest request, ResourceResponse response)
+			throws IOException {
+		PrintWriter p = response.getWriter();
+		String id = request.getResourceID();
+		if (id.equals("memory")) {
+			p.write((100 - (int) getMemoryFree()) + "");
+			p.close();
+		}
+	}
+
+	public double getMemoryFree() {
+		long physicalMemorySize = os.getTotalPhysicalMemorySize();
+		long freePhysicalMemory = os.getFreePhysicalMemorySize();
+		double free = (double) freePhysicalMemory / physicalMemorySize;
+		free = free * 100;
+		free = Math.round(free);
+		free = free / 100;
+		return free * 100;
+	}
+	public double getCpuFree(){
+		double cpu = os.getSystemCpuLoad();
+		return cpu;
+	}
+	public static void main(String[] args) {
+		monitorAction m=new monitorAction();
+		System.out.println(m.getCpuFree());
+	}
+
+}
---
+++ docroot/WEB-INF/src/com/monitoring/Monitoring.java
@@ -0,0 +1,10 @@
+package com.monitoring;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class Monitoring
+ */
+public class Monitoring extends MVCPortlet {
+	
+}
